4e2ab9638beef0fb4f4a6c398da8bc6863417de2
op3ntrap <op3ntrap@gmail.com>
2017-10-11 11:30:01 +0530

    New branch for G240

--- librarybuddy/BookManager/models.py
+++ librarybuddy/BookManager/models.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.contrib.postgres.fields import ArrayField,JSONField
+from django.contrib.postgres.fields import ArrayField, JSONField
 
 # Create your models here.
 PUBLICATION_TYPE_CHOICES = ()
@@ -13,20 +13,26 @@ class Books(models.Model):
     """
     Book Objects
     """
-    isbn = models.CharField(max_length=15, help_text="ISBN Identifier of the book")
-    asin = models.CharField(max_length=15, help_text="ASIN Identifier of the book")
-    title = models.CharField(max_length=100, help_text="Book Title")
+    isbn = models.CharField(
+        max_length=15, help_text="ISBN Identifier of the book", null=True, blank=True)
+    asin = models.CharField(
+        max_length=15, help_text="ASIN Identifier of the book", null=True, blank=True)
+    title = models.CharField(
+        max_length=100, help_text="Book Title", null=False, blank=False)
     authors = ArrayField(
         models.CharField(max_length=100)
     )
-    publisher = models.CharField(max_length=30)
+    publisher = models.CharField(max_length=30, null=True, blank=True)
     published_date = models.DateField(auto_now_add=False)
-    publication_type = models.CharField(max_length=30, choices=PUBLICATION_TYPE_CHOICES)
-    languages = models.CharField(max_length=30, choices=LANGUAGE_CHOICES)
+    publication_type = models.CharField(
+        max_length=30, choices=PUBLICATION_TYPE_CHOICES, null=False, blank=False)
+    languages = models.CharField(
+        max_length=30, choices=LANGUAGE_CHOICES, null=False, blank=False)
     cover = models.ImageField()
     tags = ArrayField(models.CharField(max_length=50))
     is_available = models.BooleanField(default=True)
-    lending_log = ArrayField(JSONField())
+    lending_log = ArrayField(
+        JSONField(blank=True, null=True), blank=True, null=True)
     """
     Format for lending log of the User class is
     lending_log = [
@@ -38,4 +44,3 @@ class Books(models.Model):
         }
     ]
     """
-
