f7873d9851b645710eec25b70fe3e7a0f0715452
Op3ntrap <op3ntrap@gmail.com>
2017-10-15 08:40:01 +0530

    Registering the models in the admin site Signed-off-by: Op3ntrap <op3ntrap@gmail.com>
    Signed-off-by: Op3ntrap <op3ntrap@gmail.com>

--- librarybuddy/TransactionManager/admin.py
+++ librarybuddy/TransactionManager/admin.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Lend, Returning
+
 
 # Register your models here.
+
+@admin.register(Lend)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('book', 'client', 'issuer', 'issue_date')
