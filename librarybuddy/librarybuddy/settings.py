b78c5c1e126bad804663d613cb73cf11155be038
Op3ntrap <op3ntrap@gmail.com>
2017-10-15 08:41:05 +0530

    Adding admindocs and relevant middle ware
    Signed-off-by: Op3ntrap <op3ntrap@gmail.com>

--- librarybuddy/librarybuddy/settings.py
+++ librarybuddy/librarybuddy/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.admindocs',
     'UserManager.apps.UsermanagerConfig',
     'BookManager.apps.BookmanagerConfig',
     'TransactionManager.apps.TransactionmanagerConfig',
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.admindocs.middleware.XViewMiddleware',
 ]
 
 ROOT_URLCONF = 'librarybuddy.urls'
