66152a0895759b01ef661fdbf13b9f3707754ddb
Op3ntrap <op3ntrap@gmail.com>
2017-10-15 08:36:28 +0530

    Adding the ModelAdmin Classes
    Signed-off-by: Op3ntrap <op3ntrap@gmail.com>

--- librarybuddy/BookManager/models.py
+++ librarybuddy/BookManager/models.py
@@ -4,16 +4,38 @@ import uuid
 from django.db import models
 from django.contrib.postgres.fields import ArrayField, JSONField
 
-# Create your models here.
+
+def archive_url_json():
+    urls = {
+        'libgen'      : "",
+        'google_books': "",
+        'amazon'      : "",
+        'publisher'   : "",
+        'doc_self'    : "",
+        'author'      : "",
+        'social_media': {
+            'facebook': "",
+            'twitter' : ""
+        }
+    }
+    return urls
 
 
 class Archive(models.Model):
     """
     Abstract Class: All Library Objects
+    from django.utils.html import format_html
+    def colored_name(self):
+        return format_html(
+        '<span style="color: #{};">{} {}</span>',
+        self.color_code,
+        self.first_name,
+        self.last_name,
+        )
     """
+    # Field Choice Tuples
     LANGUAGE_CHOICES = (
         ('EN', 'English'),
-        ('EN-IN', 'EN-India'),
         ('HIN', 'Hindi'),
         ('others', 'Others')
     )
@@ -22,37 +44,75 @@ class Archive(models.Model):
         ('b', 'B'),
         ('c', 'C'),
         ('d', 'D'),
-        ('all', 'All')
     )
-    IDENTIFIER_CHOICES = ()
-    primary_id = models.UUIDField(
-        primary_key=True, default=uuid.uuid4, editable=False)
+    IDENTIFIER_CHOICES = (
+        ('isbn', 'ISBN'),
+        ('asin', 'ASIN'),
+        ('issn', 'ISSN'),
+        ('doi', 'DOI'),
+        ('lccn', 'LCCN'),
+        ('oclc', 'OCLC')
+    )
+
+    def __str__(self):
+        author = ""
+        for val in self.authors:
+            if len(self.authors) == 1:
+                author = self.authors[0]
+                break
+            author += val + " , "
+        return "{} by {}".format(self.title, author)
+
+    # Internal Reference
+    _id = models.UUIDField(
+            primary_key=True, default=uuid.uuid4, editable=False)
+    last_modified_on = models.DateTimeField(auto_now=True, auto_created=True)
+
+    lending_log = ArrayField(
+            JSONField(null=True, editable=False),
+            blank=True, null=True, editable=False)
+    # MetaData Fields
     title = models.CharField(
-        max_length=100, help_text="Book Title", null=False, blank=False)
+            max_length=100, help_text="Book Title", null=False, blank=False)
     authors = ArrayField(
-        models.CharField(max_length=100, default="Unknown Author")
+            models.CharField(max_length=100,
+                             default="Unknown Author",
+                             help_text='Enter the Author names separated by a comma: <br>Eg. good,bad <br>For Tags '
+                                       'which contain spaces can be entered by surrounding them with double quotes. '
+                                       '<br>Eg."Risan Raja"')
     )
     publisher = models.CharField(max_length=30, null=True, blank=True)
     published_date = models.DateField(auto_now_add=False)
     languages = models.CharField(
-        max_length=30, choices=LANGUAGE_CHOICES, null=False, blank=False)
+            max_length=30, choices=LANGUAGE_CHOICES, null=False, blank=False)
     cover = models.ImageField()
-    tags = ArrayField(models.CharField(max_length=50, null=True, blank=True))
-    is_available = models.BooleanField(default=True)
-    lending_log = ArrayField(
-        JSONField(blank=True, null=True), blank=True, null=True)
+
+    tags = ArrayField(models.CharField(max_length=50, null=True, blank=True),
+                      help_text='Enter the relevant Tags separated by a comma: <br>Eg. good,bad <br>For Tags which '
+                                'contain spaces can be entered by surrounding them with double quotes. <br>Eg."Machine '
+                                'Learning"')
+    # Access Control Parameters
+    is_available = models.BooleanField(default=True, editable=False)
     access = models.BooleanField(default=True)
     accessibility = models.CharField(
-        max_length=10, choices=MEMBERSHIP_CHOICES, default='all')
+            max_length=10, choices=MEMBERSHIP_CHOICES, default='d', help_text='Membership Level Required')
+    # Universal Identification
     identifier = models.CharField(
-        max_length=10, choices=IDENTIFIER_CHOICES, default='isbn')
-    id_val = models.CharField(max_length=40, default="s", unique=True)
+            max_length=10, choices=IDENTIFIER_CHOICES, default='isbn', help_text='Select the type of Identifier')
+    id_val = models.CharField(max_length=13, default="654656", unique=True,
+                              help_text='Please enter the Identifier Code')
+    # Others
     penalty = models.FloatField()
+    urls = JSONField(default=archive_url_json)
+
+    copies = models.IntegerField(default=1, editable=False, verbose_name='Number of Copies')
 
     class Meta:
         abstract = True
         unique_together = ['identifier', 'id_val']
         ordering = ['title', '-published_date']
+
+    permissions = (('modify_access', 'Modify Access'), ('access_lending_logs', 'Access the Lending Logs'))
 
 
 class DigitalRecords(Archive):
@@ -81,10 +141,19 @@ class Book(Archive):
 
 
 class Magazine(Archive):
-    PERIODICITY_CHOICES = ()
+    PERIODICITY_CHOICES = (
+        ('yearly', 'Yearly'),
+        ('monthly', 'Monthly'),
+        ('fortnightly', 'Fortnightly'),
+        ('weekly', 'Weekly'),
+        ('daily', 'Daily')
+    )
     periodical_type = models.CharField(
-        max_length=39, choices=PERIODICITY_CHOICES)
-    archive_editions = ArrayField(models.DateField(), null=True, blank=True)
+            max_length=39, choices=PERIODICITY_CHOICES, help_text='Periodicity of the Magazine')
+    archive_editions = ArrayField(models.DateField(help_text='enter date in YYYY-MM-DD'), null=True, blank=True,
+                                  help_text='enter date in YYYY-MM-DD')
+    # def __str__(self):
+    #     return self.title,
 
 
 class Journal(Archive):
@@ -95,14 +164,14 @@ class Journal(Archive):
     )
     SUBJECT_CHOICES = ()
     subject = models.CharField(
-        max_length=30, choices=SUBJECT_CHOICES, help_text='Subject')
+            max_length=30, choices=SUBJECT_CHOICES, help_text='Subject')
     journal_type = models.CharField(
-        max_length=30, choices=JOURNAL_TYPE_CHOICES, help_text='Journal Type', default="Peer-Reviewed")
+            max_length=30, choices=JOURNAL_TYPE_CHOICES, help_text='Journal Type', default="Peer-Reviewed")
 
 
 class ResearchPaper(Archive):
     citations_count = models.IntegerField(default=0)
     citation_sources = ArrayField(models.CharField(
-        max_length=30, help_text='Identifier'))
+            max_length=30, help_text='Identifier'))
     journal = models.ForeignKey(
-        'Journal', on_delete=models.CASCADE, null=True)
+            'Journal', on_delete=models.CASCADE, null=True)
