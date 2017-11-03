c0288db03861077b8caad265e9bdb9e9abe41002
op3ntrap <op3ntrap@gmail.com>
2017-11-03 15:23:26 +0530

    Added Base template to the BookManager App
    Signed-off-by: Op3ntrap <op3ntrap@gmail.com>

--- librarybuddy/librarybuddy/urls.py
+++ librarybuddy/librarybuddy/urls.py
@@ -17,8 +17,11 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+                  url(r'^login.html', auth_views.login, name='login'),
+                  url(r'^logout.html', auth_views.logout, name='logout'),
                   url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
                   url(r'^admin/', admin.site.urls),
                   url(r'^', include('BookManager.urls'))
