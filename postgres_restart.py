a66351dc116f3de38536170f492a20a2e734d81b
Risan Raja <op3ntrap@gmail.com>
2018-01-18 12:34:48 +0530

    Changed the commentation structure of the script

--- postgres_restart.py
+++ postgres_restart.py
@@ -1,3 +1,17 @@
+"""
+This python script drops all the databases related to the project and recreates them.
+Following is the PostgreSQL code.
+
+                                                    ['DROP DATABASE library_buddy;',
+                                                    'CREATE DATABASE library_buddy;',
+                                                    'CREATE USER op3ntrap WITH PASSWORD 'a';',
+                                                    'ALTER ROLE op3ntrap SET client_encoding to 'utf8';',
+                                                    'ALTER ROLE op3ntrap SET timezone TO 'UTC+5:30';',
+                                                    'GRANT ALL PRIVILEGES ON DATABASE library_buddy TO op3ntrap;']
+
+"""
+
+
 import psycopg2
 try:
     connn = psycopg2.connect(
@@ -6,15 +20,7 @@ except:
     print "script failed"
     exit()
 conn = connn.cursor()
-"""
-['DROP DATABASE library_buddy;',
-'CREATE DATABASE library_buddy;',
-'CREATE USER op3ntrap WITH PASSWORD 'a';',
-'ALTER ROLE op3ntrap SET client_encoding to 'utf8';',
-'ALTER ROLE op3ntrap SET timezone TO 'UTC+5:30';',
-'GRANT ALL PRIVILEGES ON DATABASE library_buddy TO op3ntrap;']
 
-"""
 connn.set_isolation_level(0)
 conn.execute("""DROP DATABASE library_buddy""")
 conn.execute("""CREATE DATABASE library_buddy""")
